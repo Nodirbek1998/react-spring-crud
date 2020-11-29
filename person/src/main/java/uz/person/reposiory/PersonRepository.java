e4a8f643e8348b3bc2a904e2a21528c2314eb4e9
Nodirbek Mamadaliyev <nodirbekmamadaliyev@gmail.com>
2020-11-29 22:03:48 +0300

    delete

--- person/src/main/java/uz/person/reposiory/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package uz.person.reposiory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.person.entity.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-        boolean existsByEmail(String email);
-        boolean existsByEmailAndIdNot(String email, Integer id);
-}
